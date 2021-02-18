fd834d535a70e21bd9e3ff580ada6544ae277b6e
Joe <jparkkennaby@gmail.com>
2021-02-18 14:21:23 +0000

    2.2 - setup axios defaults. set the baseUrl to use the rem rest api

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
+import axios from 'axios';
+
+axios.defaults.withCredentials = true;
+axios.defaults.baseURL = 'http://rem-rest-api.herokuapp.com/api/';
 
 ReactDOM.render(
   <React.StrictMode>
