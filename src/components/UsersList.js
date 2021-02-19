0ac107368b8a9552521516a2a2387c7c5fa761d3
Joe <jparkkennaby@gmail.com>
2021-02-19 18:09:08 +0000

    4.3 - update ListItem UI - order returned names. print frist and last name. setup delete button

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,11 +1,29 @@
 import React from 'react';
-import { ListGroup, ListGroupItem } from 'reactstrap'
+import { ListGroup, ListGroupItem, Button } from 'reactstrap'
+
+const sortNames = (a, b) => {
+
+    if (a.firstName > b.firstName) return 1;
+    else if (a.firstName < b.firstName) return -1;
+    else if (a.lastName > b.lastName) return 1;
+    else if (a.lastName < b.lastName) return -1;
+    return 0;
+}
 
 const UsersList = ({ users }) => {
     return (
         <ListGroup>
-            {users.map(user => {
-                return <ListGroupItem key={user.id}>{user.firstName}</ListGroupItem>
+            {users.sort(sortNames).map(user => {
+                return <ListGroupItem key={user.id}>
+                    <section style={{ display: 'flex', margin: 'auto 0' }}>
+                        <div style={{ flexGrow: 1 }}>
+                            {user.firstName} {user.lastName}
+                        </div>
+                        <div>
+                            <Button outline color='danger'>Delete</Button>
+                        </div>
+                    </section>
+                </ListGroupItem>
             })}
         </ListGroup>
     );
