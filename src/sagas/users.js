a387533d77bbda2c94778d46f185152ff6ceb26d
Joe <jparkkennaby@gmail.com>
2021-02-20 10:15:05 +0000

    5.2 - setup delete user watcher and works sagas. setup new deleteUser api function.

--- src/sagas/users.js
+++ src/sagas/users.js
@@ -2,7 +2,7 @@
  * Sagas
  * Good Practice: align your saga structure similar to the redux acations
  */
-import { takeEvery, takeLatest, call, fork, put } from 'redux-saga/effects';
+import { takeEvery, takeLatest, take, call, fork, put } from 'redux-saga/effects';
 
 import * as actions from '../actions/users';
 import * as api from '../api/users';
@@ -55,9 +55,39 @@ function* watchCreateUserRequest() {
     yield takeLatest(actions.Types.CREATE_USER_REQUEST, createUser)
 }
 
+/**
+ * Delete User
+ */
+
+function* deleteUser({ userId }) {
+
+    try {
+
+        // delete user data
+        yield call(api.deleteUser, userId);
+
+        // re-request getUser saga (shouldn't we dispatch the action again here instead?)
+        yield call(getUsers);
+
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+function* watchDeleteUserRequest() {
+    while (true) {
+        const action = yield take(actions.Types.DELETE_USER_REQUEST,);
+        yield call(deleteUser, {
+            userId: action.payload.userId
+        })
+    }
+}
+
+
 const usersSagas = [
     fork(watchGetUsersRequest),
-    fork(watchCreateUserRequest)
+    fork(watchCreateUserRequest),
+    fork(watchDeleteUserRequest)
 ];
 
 export default usersSagas;
